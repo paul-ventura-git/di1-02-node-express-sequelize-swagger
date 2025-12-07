0e34dea36dcff0331c63701d9087bdaffa70a4f3
paul-ventura-git <j.paul.ventura@gmail.com>
2025-12-06 22:40:42 -0500

    02

--- config/config.js
+++ config/config.js
@@ -19,7 +19,6 @@ module.exports = {
   }
 };
 
-*/
 
 require('dotenv').config();
 const path = require('path');
@@ -42,5 +41,32 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
+  }
+};
+*/
+
+require('dotenv').config();
+const path = require('path');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'node_sequelize',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3307,
+    dialect: 'mysql',
+    logging: false
+  },
+
+  test: {
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false
+  },
+
+  production: {
+    use_env_variable: 'DATABASE_URL', // ejemplo: mysql://user:pass@host:port/dbname
+    dialect: 'mysql'
   }
 };
