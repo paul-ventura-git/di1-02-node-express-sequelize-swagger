ae740829fe209bb1674af6676e57d521daa09758
paul-ventura-git <j.paul.ventura@gmail.com>
2026-04-14 08:04:28 -0500

    06

--- config/config.js
+++ config/config.js
@@ -20,6 +20,7 @@ module.exports = {
 };
 */
 
+/*
 require('dotenv').config();
 const path = require('path');
 
@@ -43,6 +44,7 @@ module.exports = {
     dialect: 'postgres'
   }
 };
+*/
 
 /*
 require('dotenv').config();
@@ -71,3 +73,38 @@ module.exports = {
   }
 };
 */
+
+require('dotenv').config();
+const path = require('path');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'sa',
+    password: process.env.DB_PASS || '123',
+    database: process.env.DB_NAME || 'node_sequelize',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 1433,
+    dialect: 'mssql', // Dialecto para SQL Server
+    logging: false,
+    dialectOptions: {
+      options: {
+        encrypt: true, // Necesario si usas Azure o redes seguras
+        trustServerCertificate: true // Útil para entornos de desarrollo local
+      }
+    }
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'mssql',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    }
+  }
+};
